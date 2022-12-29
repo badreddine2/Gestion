3d037b3d040cf071baed37fc03a2df9d386d35c4
Badr-Eddine Aji <badreddine.aji@gmail.com>
2022-12-29 20:31:04 +0100

    plot to show depences

--- data.py
+++ data.py
@@ -1,15 +1,30 @@
 import sqlite3
-
+import matplotlib.pyplot as plt
 conn = sqlite3.connect("revenu.db")
 c = conn.cursor()
 
-c.execute(''' SELECT sum(Dette)   FROM Revenu WHERE Dette>=0 ''')
-c.execute(''' SELECT sum(Epargne)   FROM Revenu ''')
+
+#c.execute(''' SELECT Mois as m ,(Salaire-Epargne-Bouf-Mom-Fun-Dette-Loyer) as Reste FROM Revenu_2023 ''')
+
+c.execute('''SELECT Mois as Mois ,(Bouf+Mom+Fun+Dette+Loyer)  as Depenses, Salaire-(Epargne+Bouf+Mom+Fun+Dette+Loyer) as Reste from Revenu_2023''')
+dette = c.fetchall() ;
+result = dette[0]
+#print(dette[0][0])
+
+Mois = {}
+for i in range(len(dette)):
+    Mois[dette[i][0]] = dette[i][1]
+print(Mois)
 
 
 
-result = c.fetchone();
-print(result[0])
+
+
+graph = {'Salaire':2229,'Epagne':500,'Bouf': 200, 'Mom':160, 'Fun':53,'Dette' : 130, 'Loyer': 850}
+
+
+
+
 
 
 conn.commit()
--- plot.py
+++ plot.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
 
-data = {'apples': 10, 'oranges': 15, 'lemons': 5, 'limes': 20}
+data = {'Janvier': 1393, 'Février': 1413, 'Mars': 1313, 'Avril': 1313, 'Mai': 1313, 'Juin': 1263, 'Juillet': 1263, 'Aout': 1313, 'Septembre': 1263, 'Octobre': 1283, 'Novembre': 1313, 'Décembre': 1263}
+
+moyenne =0
+
+for i in data :
+    moyenne+=data[i]
+print(moyenne/12)
+
 names = list(data.keys())
 values = list(data.values())
 
 fig, ax = plt.subplots()
 ax.bar(names, values)
 plt.show()
+
